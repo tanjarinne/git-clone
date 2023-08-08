d6af4077cebffa75d1e0b82ae7ff51cc67754f60
Tanja Alva <tanja.alva@gmail.com>
2023-08-08 15:38:22 +0800

    Add initial (failing) tests for the hostname extraction lib

--- src/url.rs
+++ src/url.rs
@@ -9,3 +9,24 @@ pub fn extract_hostname(url: &str) -> Option<String> {
 
   None
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_extract_hostname() {
+    let test_cases: Vec<(&str, Option<&str>)> = vec![
+      ("https://www.example.com/path/to/something", Some("www.example.com")),
+      ("ssh://username@hostname:22/path/to/resource", Some("hostname")),
+      ("git@github.com:owner/repo.git", Some("github.com")),
+      ("invalid", None),
+      ("", None),
+    ];
+
+    for (input, expected) in test_cases {
+      let result = extract_hostname(input);
+      assert_eq!(result.as_deref(), expected);
+    }
+  }
+}
