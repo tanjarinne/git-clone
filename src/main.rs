cc97bd26ed2a5f09bc054d0a84ac35a54903bc6a
Tanja Alva <tanja.alva@gmail.com>
2023-08-08 16:37:12 +0800

    Add a function to grab gitconfigs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 
 mod url;
+pub mod git;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
   if let Some(host) = url::extract_hostname(&args[1]) {
     println!("Git host: {}", host);
+  }
+
+  if let Ok(Some(root_path)) = git::root_dir() {
+    println!("Root path: {}", root_path);
   }
 }
