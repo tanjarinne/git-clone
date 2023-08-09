e30af3f5422470ac75dbc8b697ea53ecffcf4b17
Tanja Alva <tanja.alva@gmail.com>
2023-08-09 23:37:57 +0800

    Revert fmt

--- src/git.rs
+++ src/git.rs
@@ -3,10 +3,10 @@ use git2::Config;
 const CLONE_ROOT_KEY: &str = "tanjarinne.git-clone.root-path";
 
 fn read_from_gitconfig(key: &str) -> Result<Option<String>, git2::Error> {
-    let config = Config::open_default()?;
-    config.get_string(key).map(Some).or(Ok(None))
+  let config = Config::open_default()?;
+  config.get_string(key).map(Some).or(Ok(None))
 }
 
-pub fn root_dir() -> Result<Option<String>, git2::Error> {
-    read_from_gitconfig(CLONE_ROOT_KEY)
+pub fn root_dir()  -> Result<Option<String>, git2::Error> {
+  read_from_gitconfig(CLONE_ROOT_KEY)
 }
